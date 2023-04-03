21e08b06c627f3fdf699ee17113e43ce8098658f
Littlepippo <128298325+Littlepippo@users.noreply.github.com>
2023-04-02 17:35:13 -0700

    Add files via upload

---
+++ script .js
@@ -0,0 +1,67 @@
+
+var order1=document.getElementById("order1")
+var order2=document.getElementById("order2")
+var order3=document.getElementById("order3")
+var order4=document.getElementById("order4")
+var order5=document.getElementById("order5")
+var order6=document.getElementById("order6")
+var order7=document.getElementById("order7")
+var order8=document.getElementById("order8")
+var order9=document.getElementById("order9")
+var order10=document.getElementById("order10")
+var order11=document.getElementById("order11")
+var order12=document.getElementById("order12")
+var order13=document.getElementById("order13")
+var order14=document.getElementById("order14")
+var order15=document.getElementById("order15")
+var order16=document.getElementById("order16")
+var order17=document.getElementById("order17")
+var order18=document.getElementById("order18")
+var order19=document.getElementById("order19")
+var order20=document.getElementById("order20")
+
+
+function search(){let result =document.getElementById("result").value; report();
+function report(){document.open();document.write('<center><div><h3 style="color:red;">No Search result for</h3></div></center>'+result);document.close()}} 
+
+
+order1.onclick = function(){document.write("man")}
+
+order2.onclick = function(){document.write("man")}
+
+order3.onclick = function(){document.write("man")}
+
+order4.onclick = function(){document.write("man")}
+
+order5.onclick = function(){document.write("man")}
+
+order6.onclick = function(){document.write("man")}
+
+order7.onclick = function(){document.write("man")}
+
+order8.onclick = function(){document.write("man")}
+
+order9.onclick = function(){document.write("man")}
+
+order10.onclick = function(){document.write("man")}
+
+order11.onclick = function(){document.write("man")}
+
+order12.onclick = function(){document.write("man")}
+
+order13.onclick = function(){document.write("man")}
+
+order14.onclick = function(){document.write("man")}
+
+order15.onclick = function(){document.write("man")}
+
+order16.onclick = function(){document.write("man")}
+
+order17.onclick = function(){document.write("man")}
+
+order18.onclick = function(){document.write("man")}
+
+order19.onclick = function(){document.write("man")}
+
+order20.onclick = function(){document.write("man")}
+
